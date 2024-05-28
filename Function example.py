0b22b19458de6701a96b809005c5bf8b9ef9bad5
Kung0301 <kung.positive.mindset@gmail.com>
2024-05-28 17:20:06 +0700

    work update

---
+++ Function example.py
@@ -0,0 +1,10 @@
+def say_Hello_World() :
+    print("Hello World")
+    say_hello()
+    print("Woooooo")
+def say_hello() :
+    print("Hello Kung")
+    print("hahahahaha")
+    say_Hello_World()
+say_Hello_World()
+say_hello()
---
+++ Function param example.py
@@ -0,0 +1,12 @@
+def plus_number(x,y) :
+    print(x+y)
+def minus_number(x,y) :
+    print(x-y)
+def times_number(x, y):
+    print(x*y)
+def devide_number(x, y):
+    print(x/y)
+plus_number(10,5)
+minus_number(20,2)
+times_number(2,4)
+devide_number(8,2)
