2d5d14c3fb43c9b5dde6c118fafc3c2a859bdfc2
Kung0301 <kung.positive.mindset@gmail.com>
2024-06-13 22:49:20 +0700

    update

--- Currency example.py
+++ Currency example.py
@@ -1,6 +1,28 @@
 from currency_converter import CurrencyConverter
-from tkinter import *
-class Avalue
-mainWindow = Tk()
-button1 = Button(mainWindow,text="Input price :",command=)
 c = CurrencyConverter()
+print("นี่คือโปรแกรมเปรียบเทียบสกุลเงิน")
+print("โปรดใส่จำนวนเงิน")
+value = float(input())
+def exchange() :
+    print("โปรดเลือกสกุลเงิน :")
+    print("USD")
+    print("CNY")
+    print("JPY")
+    print("SGD")
+    print("GBP")
+    choose = input("สกุลเงินที่เลือก :")
+    if choose == "USD" :
+        print(c.convert(value,'THB','USD'), "USD")
+    elif choose == "CNY" :
+        print(c.convert(value,'THB','CNY'), "CNY")
+    elif choose == "JPY" :
+        print(c.convert(value,'THB','JPY'), "JPY")
+    elif choose == "SGD" :
+        print(c.convert(value,'THB', 'SGD'), "SGD")
+    elif choose == "GBP" :
+        print(c.convert(value,'THB','GBP'), "GBP")
+    else :
+        print("โปรดทำรายการอีกครั้ง")
+        return exchange()
+exchange()
+print("ทำรายการเสร็จสิ้น")
